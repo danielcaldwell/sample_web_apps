565e9f58ec5a3bb328e208fa33690221feb1ce5a
Daniel Caldwell <daniel@danielcaldwell.com>
2020-02-29 00:10:45 -0800

    added newline to make curl requests print better

--- python_helloworld/docker/webapp.py
+++ python_helloworld/docker/webapp.py
@@ -5,7 +5,7 @@ app = Flask( __name__ )
 
 @app.route('/')
 def hello():
-    return 'Hello Python World!'
+    return 'Hello Python World!\n'
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port='5000', debug=True)
